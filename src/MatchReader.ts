831ec4277311266d2a7351e3324dc3d27637b63f
NikDoe <nikd0e@yandex.ru>
2022-07-20 19:23:29 +0300

    refactor(stats): extracting match references

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,7 +1,12 @@
+import { dateStringToDate } from './utils';
+import { MatchResults } from './MatchResults';
+
 interface DataReader {
 	read(): void;
 	data: string[][];
 }
+
+type MatchData = [Date, string, string, number, number, MatchResults, string];
 
 export class MatchReader {
 	constructor(public reader: DataReader) {}
