8cd53b6e880209d53c5857253341df6af565858c
NikDoe <nikd0e@yandex.ru>
2022-07-23 15:22:26 +0300

    feature(stats): this fixes #6, implementing an analyzer of wins games
    -extracting the logic from index.ts
    -avoid a specific team named for the variable

---
+++ src/analyzers/WinsAnalysis.ts
@@ -0,0 +1,20 @@
+import { Analyzer } from '../Summary';
+import { MatchData } from '../MatchData';
+import { MatchResults } from '../MatchResults';
+
+export class WinsAnalysis implements Analyzer {
+	constructor(public team: string) {}
+
+	run(matches: MatchData[]): string {
+		let wins = 0;
+
+		for (const match of matches) {
+			if (match[1] === this.team && match[5] === MatchResults.HomeWin) {
+				wins++;
+			} else if (match[2] === this.team && match[5] === MatchResults.AwayWin) {
+				wins++;
+			}
+		}
+		return `team ${this.team} won ${wins} games`;
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { MatchResults } from './MatchResults';
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
 
@@ -7,14 +6,4 @@ const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-let arsenalWins = 0;
-
-for (const match of matchReader.matches) {
-	if (match[1] === 'Arsenal' && match[5] === MatchResults.HomeWin) {
-		arsenalWins++;
-	} else if (match[2] === 'Arsenal' && match[5] === MatchResults.AwayWin) {
-		arsenalWins++;
-	}
-}
-
-console.log(`Arsenal won ${arsenalWins} games`);
+console.log(`Arsenal won games`);
